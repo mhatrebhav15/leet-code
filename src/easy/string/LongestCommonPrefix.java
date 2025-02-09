aeba7b204a76a376724da17c3b3fa522a8d41ba3
Bhavesh <mhatrebhav15@gmail.com>
2025-02-09 16:06:11 +0530

    14. Longest Common Prefix

---
+++ src/easy/string/LongestCommonPrefix.java
@@ -0,0 +1,24 @@
+package easy.string;
+
+public class LongestCommonPrefix {
+
+	public static String longestCommonPrefix(String[] strs) {
+		String prefix = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+			while (strs[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length() - 1);
+				if ("".equals(prefix)) {
+					return "";
+				}
+			}
+		}
+		return prefix;
+	}
+
+	public static void main(String[] args) {
+		String[] strs = { "flower", "flow", "flight" };
+		String longestCommonPrefix = longestCommonPrefix(strs);
+		System.out.println(longestCommonPrefix);
+	}
+
+}
